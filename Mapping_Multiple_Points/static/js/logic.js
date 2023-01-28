2e591c5faf95b9d97652b3a6cce9ddc035ede31d
Kyle <kyle.brumbaugh9@gmail.com>
2023-01-28 16:00:40 -0500

    pushing multiple cities markers

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,8 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 //Create the map object with a center and zoom level.
-//let map = L.map('mapid').setView([40.7, -94.5], 4);
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
 
 // This does the same thing as the setView method above// Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
@@ -23,15 +23,19 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     accessToken: API_KEY
 });
 
+// Get data from cities.js
+let cityData = cities;
 
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
-L.circle([34.0522, -118.2437], {
-    radius: 100,
-    color: "black",
-    fillColor: "yellow",
-    fillOpacity: 0.2
- }).addTo(map);
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+   });
+
 
 
 // Then we add our 'graymap' tile layer to the map.
